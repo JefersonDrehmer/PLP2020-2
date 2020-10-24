14d41762fe183c4efdc8fd2d5ff009caf86dcde4
JefersonDrehmer <73262758+JefersonDrehmer@users.noreply.github.com>
2020-10-23 22:07:40 -0300

    Add files via upload

---
+++ EXE1/Funcao.hs
@@ -0,0 +1,18 @@
+---1. Defina uma função para calcular o quadrado do dobro do seu argumento.
+quadradoDobro x = (x + x)^2
+
+---2. Defina uma função para calcular o dobro do quadrado do seu argumento.
+dobroQuadrado x = (x^2) *2
+
+---3. Faça uma função que receba três notas de um aluno e calcule a média aritmética das notas.
+media n1 n2 n3 = (n1 + n2 + n3) /3
+
+---4. Sabe-se que o valor do quilowatt de energia elétrica custa um quinto do salário mínimo.
+-- Defina uma função que receba o valor do salário mínimo e a 
+---quantidade de quilowatts consumida por uma residência, e resulta no valor a ser pago
+---com desconto de 15%.
+conta sal qlw =  y - x
+            where
+                 valor= sal / 5
+                 y= valor * qlw
+                 x = y * 0.15             
